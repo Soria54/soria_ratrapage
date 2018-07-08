dc15c46b5b79ec1409f89e1a0f76d006a2c6a887
soria54 <soria.cao@hotmail.com>
2018-07-08 18:32:25 +0200

    mur

--- view/src/main/java/view/Grille.java
+++ view/src/main/java/view/Grille.java
@@ -18,6 +18,12 @@ public class Grille extends JPanel implements Observer{
 	private int joueur1y;
 	private int joueur2x;
 	private int joueur2y;
+	
+	private int Nbwall;
+	private int wall1x[];
+	private int wall1y[];
+	private int wall2x[];
+	private int wall2y[];
 /**
 	 * 
 	 */
@@ -49,19 +55,21 @@ public void paintComponent(Graphics g){
 		g.drawLine(0+11, uniteY*i+2, uniteX*30+11, uniteY*i+2);
 		}
 		
+
 		
 		//j1
-		//moto1 = this.model.getMoto1();
-		
-		g.setColor(Color.blue);
-		g.fillRect(this.joueur1x,this.joueur1y, 20, 20);
+		Cube Cube1 = new Cube(Color.blue);
+		Cube1.setLocation(this.joueur1x, this.joueur1y);
+		this.add(Cube1);
 		
 		//j2
-		g.setColor(Color.red);
-		g.fillRect(this.joueur2x,this.joueur2y, 20, 20);
+		Cube Cube2 = new Cube(Color.red);
+		Cube2.setLocation(this.joueur2x, this.joueur2y);
+		this.add(Cube2);
 		
 		
 }
+
 
 @Override
 public void update(Observable observable, Object o) 
@@ -90,7 +98,25 @@ public void setJoueur2y(int joueur2y) {
 
 
 
+public void setNbwall(int nbwall) {
+	this.Nbwall = nbwall;
+}
 
+public void setWall1x(int wall1x) {
+	this.wall1x[Nbwall] = wall1x;
+}
+
+public void setWall1y(int wall1y) {
+	this.wall1y[Nbwall] = wall1y;
+}
+
+public void setWall2x(int wall2x) {
+	this.wall2x[Nbwall] = wall2x;
+}
+
+public void setWall2y(int wall2y) {
+	this.wall2y[Nbwall] = wall2y;
+}
 
 }
 
